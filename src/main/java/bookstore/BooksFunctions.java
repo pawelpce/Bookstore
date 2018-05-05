4750eb3e7c98612fff2fbc7699031613797f619f
Pawel Banasiak <pawelpce@gmail.com>
2018-05-05 09:00:46 +0200

    BookstoreBeforeSecondWeekend

---
+++ src/main/java/bookstore/BooksFunctions.java
@@ -0,0 +1,183 @@
+package bookstore;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BooksFunctions {
+
+    private List<Book> books = Book.importBooks();
+
+    public Book getBookByIsbn(String isbn) {
+
+        List<Book> filteredBooks;
+        Book bookWithGivenIsbn;
+
+        filteredBooks = books.stream()
+                .filter(book -> book.getIsbn().equals(isbn))
+                .collect(Collectors.toList());
+
+        bookWithGivenIsbn = filteredBooks.get(0);
+
+        return bookWithGivenIsbn;
+    }
+
+    public List<Book> sortBookListByYear() {
+
+        List<Book> sortedBooks = books.stream()
+                .sorted(Comparator.comparing(Book::getYear))
+                .collect(Collectors.toList());
+
+        return sortedBooks;
+
+    }
+
+    public List<Book> getLastTwoBooks() {
+
+        List<Book> sortedBooks = books.stream()
+                .sorted(Comparator.comparing(Book::getYear))
+                .collect(Collectors.toList());
+
+        sortedBooks = sortedBooks.subList(sortedBooks.size() - 2, sortedBooks.size());
+        return sortedBooks;
+    }
+
+    public List<Book> sortBookListByYearFromTheLatest() {
+
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getYear).reversed())
+                .collect(Collectors.toList());
+
+    }
+
+    public Book returnTheFirstBook() {
+
+        Book theFirst = books.stream()
+                .sorted(Comparator.comparing(Book::getYear))
+                .findFirst()
+                .get();
+
+        return theFirst;
+
+    }
+
+    public Book returnTheLatestBook() {
+
+        Book theLatest = books.stream()
+                .sorted(Comparator.comparing(Book::getYear).reversed())
+                .findFirst()
+                .get();
+
+        return theLatest;
+
+    }
+
+    public int sumOfYears() {
+        return books.stream().mapToInt(value -> value.getYear()).sum();
+//        int sum = 0;
+//
+//        for (int i = 0; i < books.size(); i++){
+//
+//            sum = sum + books.get(i).getYear();
+//
+//        }
+//        return sum;
+    }
+
+    public int numberOfBooksPublishedAfter2007() {
+
+        List<Book> sortedList = books.stream()
+                .filter(book -> book.getYear() > 2007)
+                .collect(Collectors.toList());
+
+        return sortedList.size();
+
+    }
+
+    public boolean doAllBooksPublishedAfter2000() {
+
+        List<Book> filteredList = books.stream()
+                .filter(book -> book.getYear() > 2000)
+                .collect(Collectors.toList());
+
+        return books.size() == filteredList.size();
+    }
+
+    public double averageYearOfPublishedBooks() {
+
+        double average = books.stream()
+                .mapToDouble(Book::getYear).average().getAsDouble();
+
+        return average;
+    }
+
+    public boolean doAnyOfBooksPublishedBefore2000() {
+
+        return books.stream().anyMatch(book -> book.getYear() < 2000);
+//
+//        List<Book> filteredList = books.stream()
+//                .filter(book -> book.getYear() < 2000)
+//                .collect(Collectors.toList());
+//
+//        return filteredList.size() > 0;
+    }
+
+    public List<Book> booksStartedWithTAndPublishedAfter2009() {
+
+        List<Book> filteredBookList = books.stream()
+                .filter(book -> book.getTitle().startsWith("T"))
+                .filter(book -> book.getYear() > 2009)
+                .collect(Collectors.toList());
+
+        return filteredBookList;
+    }
+
+    public List<Book> adding100YearsToAllBooks() {
+        books.stream().forEach(book -> book.setYear(book.getYear() + 100));
+        return books;
+
+        /*List<Book> modifiedBooks = books;
+
+        for (int i = 0; i < books.size(); i++){
+            modifiedBooks.get(i).setYear(books.get(i).getYear() + 100);
+        }
+
+        return modifiedBooks;*/
+
+    }
+
+    public List<Book> booksWithPublishedYearDivisibleBy2() {
+
+        List<Book> modifiedList = books.stream()
+                .filter(book -> book.getYear() % 2 == 0)
+                .collect(Collectors.toList());
+
+        return modifiedList;
+
+    }
+
+    public List<Book> addToEveryTitleWordsFirstEdition() {
+
+        books.stream().forEach(book -> book.setTitle(book.getTitle() + " FIRST EDITION"));
+
+        return books;
+    }
+
+
+    public static void main(String[] args) {
+
+        List<Book> secondList = Book.importBooks();
+
+        for (int i = 0; i < secondList.size(); i++) {
+
+            if (secondList.get(i).getYear() == Book.importBooks().get(i).getYear()) {
+                System.out.println(true);
+            } else {
+                System.out.println(false);
+            }
+
+        }
+
+    }
+
+}
